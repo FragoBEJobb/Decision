495e682b4df2a6c71c4471ec4a23a9f17ad7d4df
Francisco Gonzalez <francisco.gonzalez.costa@gmail.com>
2019-10-08 18:31:18 +0200

    This is a commit, Hooray!

--- Decisions/Program.cs
+++ Decisions/Program.cs
@@ -10,20 +10,45 @@ namespace Decisions
     {
         static void Main(string[] args)
         {
+            /*
             Console.WriteLine("Bob's Big Giveaway");
             Console.Write("Coose a door: 1, 2, or 3: ");
             string userValue = Console.ReadLine();
 
+            string message = "";
+
             if (userValue == "1")
             {
-                string message = ("You won a new car!");
-                Console.WriteLine(message);
+                message = ("You won a new car!");                
             }
-
+            else if (userValue == "2")
+            {
+                message = "You won a new boat!";
+            }
+            else if (userValue == "3")
+            {
+                message = "You don a new cat!";
+            }
+            else
+            {
+                message = "Sorry, we didn't understand. ";
+                message += "You lose.";
+            }
+            Console.WriteLine(message);
             Console.ReadLine();
+            */
 
+            Console.WriteLine("Bob's Big Giveaway");
+            Console.Write("Coose a door: 1, 2, or 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+
+            console.write("you won a ");
+            console.write(message);
+            console.write(".");
             
-
+            Console.ReadLine();
         }
     }
 }
